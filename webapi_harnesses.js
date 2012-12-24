7ae95cd82b2c9eade63c52c0fd2d0f2547640b6e
jds2501 <jsmith@mozilla.com>
2012-12-23 19:27:01 -0500

    Initial commit

---
+++ webapi_harnesses.js
@@ -0,0 +1,84 @@
+function contactTest() {
+  var contact = new mozContact();
+  contact.init({name: "Tom"});
+  var request = navigator.mozContacts.save(contact);
+	
+	var output = document.getElementById('contactsoutput');
+	
+  request.onsuccess = function() {
+		output.textContent = "Successful addition of a contact";
+	};
+  request.onerror = function() {
+		output.textContent = "Failed to add a contact";
+	};
+}
+
+function desktopNotificationTest() {
+	var notification = navigator.mozNotification.createNotification("Hey, check this out!");
+	notification.show();
+}
+
+function deviceStorageTest(type) {
+	var storage = navigator.getDeviceStorage(type);
+	var result = storage.enumerate();
+	var output = document.getElementById('devicestorageoutput');
+	output.textContent = "";
+
+	result.onsuccess = function(obj) {
+	
+		if(obj.target.result === null) {
+			output.textContent = output.textContent + "|| enumerate for type " + type + " has finished";
+		} else {
+			var filename = e.target.result.name;
+			output.textContent = output.textContent + "|| enumerate for type " + type + " found " + filename;
+			object.target.continue();
+		}
+	
+	};
+	
+	result.onerror = function(err) {
+		output.textContent = "enumerate failed on type " + type + " with " + err.target.error.name;
+	};
+}
+
+function fmradioTest() {
+	var antennaAvailable = navigator.mozFMRadio.antennaAvailable;
+	var enabled = navigator.mozFMRadio.enabled;
+	var frequency = navigator.mozFMRadio.frequency;
+
+	var output = document.getElementById('fmradiooutput');
+	output.textContent = "antennaAvailable: " + antennaAvailable + ", enabled: " + enabled + ", frequency: " + frequency;
+}
+
+function geolocationTest() {
+	var output = document.getElementById('geolocationoutput');
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+		output.textContent = position.coords.latitude + " " + position.coords.longitude;
+	}, function(error) {
+		output.textContent = error.message;
+	}, {enableHighAccuracy: false, timeout: 6000});
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('contacts').onclick = contactTest;
+	document.getElementById('notification').onclick = desktopNotificationTest;
+	document.getElementById('geolocation').onclick = geolocationTest;
+	document.getElementById('fmradio').onclick = fmradioTest;
+	
+	document.getElementById('device storage pictures').onclick = function() {
+		deviceStorageTest('pictures');
+	};
+
+	document.getElementById('device storage music').onclick = function() {
+		deviceStorageTest('music');
+	};
+
+	document.getElementById('device storage sdcard').onclick = function() {
+		deviceStorageTest('sdcard');
+	};
+
+	document.getElementById('device storage videos').onclick = function() {
+		deviceStorageTest('videos');
+	};
+});
