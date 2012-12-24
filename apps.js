d292554f999642c0401f170a3586b8c8c3791a76
jds2501 <jsmith@mozilla.com>
2012-12-23 20:13:32 -0500

    Added hosted app test page

---
+++ apps.js
@@ -0,0 +1,17 @@
+﻿function installHostedApp(manifest) {
+  var request = navigator.mozApps.install(manifest);
+
+  request.onsuccess = function() {
+    alert("Success installing app : " + request.result.manifest.name);
+  };
+
+  request.onerror = function() {
+    alert("Error installing app : " + request.error.name);
+  };
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('hostedappinstall').onclick = function() {
+    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/manifest.webapp");
+  };
+});
