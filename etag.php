8abb95718b5bb99e33c4fbb5570d5305d2a72993
jds2501 <jsmith@mozilla.com>
2013-01-01 19:37:09 -0500

    Add etag test

---
+++ etag.php
@@ -0,0 +1,20 @@
+﻿<?
+
+$time = 1280951171;
+$lastmod = gmdate('D, d M Y H:i:s \G\M\T', $time);
+$etag = '123';
+
+
+$ifmod = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $lastmod : null; 
+$iftag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] == $etag : null; 
+
+if (($ifmod || $iftag) && ($ifmod !== false && $iftag !== false)) { 
+    header('HTTP/1.0 304 Not Modified'); 
+} else {
+    header("Last-Modified: $lastmod"); 
+    header("ETag: $etag");
+}
+
+print_r($_SERVER);
+
+?>
