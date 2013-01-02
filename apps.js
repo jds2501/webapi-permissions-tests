a0dcc9cb5bceb747b8fd1c1b2bfe2ff6b1e006ba
jds2501 <jsmith@mozilla.com>
2013-01-01 20:03:17 -0500

    Break out server to constant

--- apps.js
+++ apps.js
@@ -1,4 +1,6 @@
-﻿function installHostedApp(manifest) {
+﻿var SERVER = "http://jds2501.github.com/";
+
+function installHostedApp(manifest) {
   var request = navigator.mozApps.install(manifest);
 
   request.onsuccess = function() {
@@ -11,7 +13,7 @@
 }
 
 function installPackagedApp(minimanifest) {
-	var request = navigator.mozApps.installPackage(minimanifest);
+  var request = navigator.mozApps.installPackage(minimanifest);
 
   request.onsuccess = function() {
     alert("Success installing app : " + request.result.manifest.name);
@@ -24,57 +26,57 @@ function installPackagedApp(minimanifest) {
 
 window.addEventListener("DOMContentLoaded", function() {
   document.getElementById('hostedapptc1').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/manifest.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/manifest.webapp");
   };
   document.getElementById('hostedappperms').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/manifest.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/manifest.webapp");
   };
   document.getElementById('hostedapptc2').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/noicon.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/noicon.webapp");
   };
   document.getElementById('hostedapptc3').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/japanese.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/japanese.webapp");
   };
   document.getElementById('hostedapptc4').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/developernameonly.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/developernameonly.webapp");
   };
   document.getElementById('hostedapptc5').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/developerurlonly.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/developerurlonly.webapp");
   };
   document.getElementById('hostedapptc6').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/singlelocaleoverride.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/singlelocaleoverride.webapp");
   };
   document.getElementById('hostedapptc7').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/doublelocaleoverride.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/doublelocaleoverride.webapp");
   };
   document.getElementById('hostedapptc8').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/privileged.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/privileged.webapp");
   };
   document.getElementById('hostedapptc9').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/certified.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/certified.webapp");
   };
   document.getElementById('appcachepreloadtc1').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/smallappcachepreload.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/smallappcachepreload.webapp");
   };
   document.getElementById('appcachepreloadtc2').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/mediumappcachepreload.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/mediumappcachepreload.webapp");
   };
   document.getElementById('appcachepreloadtc3').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/invalidappcachepreload.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/invalidappcachepreload.webapp");
   };
   document.getElementById('appcachepreloadtc4').onclick = function() {
-    installHostedApp("http://jds2501.github.com/webapi-permissions-tests/largeappcachepreload.webapp");
+    installHostedApp(SERVER + "webapi-permissions-tests/largeappcachepreload.webapp");
   };
-	document.getElementById('packagedapptc1').onclick = function() {
-		installPackagedApp("http://jds2501.github.com/webapi-permissions-tests/privilegedappnosigning.manifest");
-	};
-	document.getElementById('packagedapptc2').onclick = function() {
-		installPackagedApp("http://jds2501.github.com/webapi-permissions-tests/webpackagedappwithperms.manifest");
-	};
-	document.getElementById('packagedapptc3').onclick = function() {
-		installPackagedApp("http://jds2501.github.com/webapi-permissions-tests/largesizeinminimanifest.manifest");
-	};
-	document.getElementById('hostedappupdatetc1').onclick = function() {
-		installHostedApp("http://jds2501.github.com/webapi-permissions-tests/simple_appcache_update/manifest.webapp");
-	};
+  document.getElementById('packagedapptc1').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/privilegedappnosigning.manifest");
+  };
+  document.getElementById('packagedapptc2').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/webpackagedappwithperms.manifest");
+  };
+  document.getElementById('packagedapptc3').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/largesizeinminimanifest.manifest");
+  };
+  document.getElementById('hostedappupdatetc1').onclick = function() {
+    installHostedApp(SERVER + "webapi-permissions-tests/simple_appcache_update/manifest.webapp");
+  };
 });
