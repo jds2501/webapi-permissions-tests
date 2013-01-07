697f74cf011dda03a8ec9502ad34c869e168f0c5
jds2501 <jsmith@mozilla.com>
2013-01-07 14:05:21 +0100

    Added browser api test case

---
+++ samplebrowser.js
@@ -0,0 +1,16 @@
+﻿function getScreenshot() {
+	var browser = document.getElementById('browser');
+	var result = browser.getScreenshot(300, 400);
+	
+	result.onsuccess = function(obj) {
+		alert("I got a screenshot");
+	};
+
+	result.onerror = function(err) {
+		alert("I fail miserably!");
+	};
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	document.getElementById('browsertest').onclick = getScreenshot;
+});
