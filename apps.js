59c922ec9f57d368c244ade6d71a0919682bfcc8
jds2501 <jsmith@mozilla.com>
2013-01-09 10:22:37 +0100

    Added Web Version of UITests app

--- apps.js
+++ apps.js
@@ -145,6 +145,9 @@ window.addEventListener("DOMContentLoaded", function() {
   document.getElementById('packagedapptc12').onclick = function() {
     installPackagedApp(SERVER + "webapi-permissions-tests/invalidminimanifest.manifest");
   };
+  document.getElementById('packagedapptc13').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/uitestswebtype.manifest");
+  };
   document.getElementById('hostedappupdatetc1').onclick = function() {
     installHostedApp(SERVER + "webapi-permissions-tests/simple_appcache_update/manifest.webapp");
   };
