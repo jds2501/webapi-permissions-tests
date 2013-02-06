7fd340d0c77fd00d4141a167d981f5948b8d289e
jds2501 <jsmith@mozilla.com>
2013-02-06 14:27:40 -0800

    Adding signing tests

--- apps.js
+++ apps.js
@@ -160,6 +160,12 @@ window.addEventListener("DOMContentLoaded", function() {
   document.getElementById('packagedapptc15').onclick = function() {
     installPackagedApp(SERVER + "webapi-permissions-tests/badlaunchpath.manifest");
   };
+  document.getElementById('packagedapptc16').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/usersigned.manifest");
+  };
+  document.getElementById('packagedapptc17').onclick = function() {
+    installPackagedApp(SERVER + "webapi-permissions-tests/reviewersigned.manifest");
+  };
   document.getElementById('hostedappupdatetc1').onclick = function() {
     installHostedApp(SERVER + "webapi-permissions-tests/simple_appcache_update/manifest.webapp");
   };
